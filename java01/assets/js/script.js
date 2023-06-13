57344d08ae84159a80328d71b090f5bb66e37836
Gabriel Augusto de Almeida <gabriel.almeida1@alunos.sc.senac.br>
2023-06-13 08:25:19 -0300

    envio

---
+++ java01/assets/js/script.js
@@ -0,0 +1,25 @@
+alert("arquivo js ola mundo");
+alert("arquivo js ola mundo");
+alert("arquivo js  quantos ano vcs tem");
+alert("arquivo js quero muito comer sua irmã");
+alert("arquivo js vc foi hakeado");
+
+// criar variaveis, usamos o "let"
+let numero = 6;
+let meu = 4;
+let ola = 9;
+
+let soma = numero + meu;
+alert ("soma: "+soma);
+
+let subtracao = meu - ola;
+alert ("soma"+subtracao);
+
+let divisao = meu / ola;
+alert ("soma"+divisao);
+
+let multiplicacao = meu * ola;
+alert ("soma"+multiplicacao);
+
+let resto = meu % ola;
+alert ("soma"+resto);
